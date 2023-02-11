b11962d3d761e4c2247f87c62654fae13dea7dd4
Bella <Sahnazaroveduard@gmail.com>
2023-02-10 23:50:30 -0500

    dark

---
+++ src/test/java/technostudyB7/SeleniumTasks/Task_1_Facebook.java
@@ -0,0 +1,23 @@
+package technostudyB7.SeleniumTasks;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task_1_Facebook {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com");
+        System.out.println(driver.getTitle());
+        String title = "Facebook - log in or sign up";
+        if (driver.getTitle().equals(title)){
+            System.out.println(true);
+
+        }else {
+            System.out.println(false);
+        }
+        driver.quit();
+    }
+}
