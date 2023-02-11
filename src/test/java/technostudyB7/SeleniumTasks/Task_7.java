40a520e367c935416044b7141cf123520d79c8a3
Bella <Sahnazaroveduard@gmail.com>
2023-02-10 23:56:43 -0500

    with

---
+++ src/test/java/technostudyB7/SeleniumTasks/Task_7.java
@@ -0,0 +1,47 @@
+package technostudyB7.SeleniumTasks;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task_7 {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+
+        String expectedTitle = "Web Orders Login";
+
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+        // "USERNAME"
+        // <input name="ctl00$MainContent$username" type="text" id="ctl00_MainContent_username" class="txt" style="width:142px">
+
+        // PASSWORD
+        //<input name="ctl00$MainContent$password" type="password" id="ctl00_MainContent_password" class="txt" style="width:142px">
+
+        WebElement loginBox = driver.findElement(By.id("ctl00_MainContent_username"));
+        loginBox.sendKeys("Tester");
+
+        WebElement passwordBox = driver.findElement(By.id("ctl00_MainContent_password"));
+        passwordBox.sendKeys("test" + Keys.ENTER);
+
+        if (driver.getTitle().equals("Web Orders")){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+    }
+}
